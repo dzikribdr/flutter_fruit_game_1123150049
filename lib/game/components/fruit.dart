355fb137547e1d942c2c6edc14dca7542badfee3
dzikriabdr <dzikriabdr@gmail.com>
2026-02-14 14:34:52 +0700

    commit akhir pembuatan fruit catcher game

--- lib/game/components/fruit.dart
+++ lib/game/components/fruit.dart
@@ -10,8 +10,23 @@ class Fruit extends PositionComponent
     with HasGameRef<FruitCatcherGame>, CollisionCallbacks {
   final double speed;
 
+  late final Paint paint;
+
   Fruit({required super.position, required this.speed})
-    : super(size: Vector2.all(36));
+    : super(size: Vector2.all(36)) {
+    // Warna ditentukan SEKALI saat object dibuat
+    paint = Paint()
+      ..color = _fruitColors[Random().nextInt(_fruitColors.length)];
+  }
+
+  static const List<Color> _fruitColors = [
+    Colors.redAccent,
+    Colors.orangeAccent,
+    Colors.yellowAccent,
+    Colors.greenAccent,
+    Colors.blueAccent,
+    Colors.purpleAccent,
+  ];
 
   @override
   Future<void> onLoad() async {
@@ -39,9 +54,6 @@ class Fruit extends PositionComponent
 
   @override
   void render(Canvas canvas) {
-    final paint = Paint()
-      ..color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
-
     canvas.drawCircle(Offset(size.x / 2, size.y / 2), size.x / 2, paint);
   }
 }
